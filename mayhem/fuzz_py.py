19bf75c3563ca05939a0d07afbe0096f50068ca0
Bailey Capuano <bcapuano@asu.edu>
2022-12-28 10:23:10 -0500

    Changed error catching

--- mayhem/fuzz_py.py
+++ mayhem/fuzz_py.py
@@ -2,17 +2,23 @@
 
 import atheris
 import sys
+
 import fuzz_helpers
+import random
 
 with atheris.instrument_imports(include=['metapensiero.pj']):
     from metapensiero.pj.__main__ import transform_string
 
-
+from metapensiero.pj.processor.exceptions import TransformationError
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
         transform_string(fdp.ConsumeRemainingString())
-    except (SyntaxError, ValueError, ZeroDivisionError):
+    except SyntaxError:
+        return -1
+    except (ValueError, ZeroDivisionError, TransformationError) as e:
+        if random.random() > 0.90 and "null bytes" not in str(e):
+            raise e
         return -1
 def main():
     atheris.Setup(sys.argv, TestOneInput)
